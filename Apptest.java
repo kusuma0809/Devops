4f59e0a845548662ccf2c208b5193f560fc0ab14
kusuma0809 <kusumav433@gmail.com>
2025-05-22 11:43:12 +0530

    Add files via upload

--- Apptest.java
+++ Apptest.java
@@ -1,15 +1,18 @@
 package Dev_test;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import Dev_java.App;
 
 public class Apptest {
+	@Test
 	public void testLogin1 ()
 	{
 		App myapp=new App();
-			Assert.assertEquals(0,myapp.userLogin("abc","ab123"));
+			Assert.assertEquals(1,myapp.userLogin("abc","ab123"));
 		}
+	@Test
 public void testLogin2 ()
 		{
 			App myapp2=new App();
